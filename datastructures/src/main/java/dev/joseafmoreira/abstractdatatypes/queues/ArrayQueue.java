d40125a1fa3d6dad1cbc47fb544a71fc54a3cf44
José Moreira <52895465+joseafmoreira@users.noreply.github.com>
2024-10-18 15:24:29 +0100

    Implemented Array Stack and Queue

---
+++ datastructures/src/main/java/dev/joseafmoreira/abstractdatatypes/queues/ArrayQueue.java
@@ -0,0 +1,127 @@
+package dev.joseafmoreira.abstractdatatypes.queues;
+
+import dev.joseafmoreira.exceptions.EmptyCollectionException;
+import pt.ipp.estg.ed.QueueADT;
+
+/**
+ * Resizable-array implementation of the {@code QueueADT} interface.
+ * 
+ * <h3>ArrayQueue</h3>
+ * @param <T> the type of element stored in this queue
+ * @since 1.0
+ * @version 1.0
+ * @author joseafmoreira
+ * @see QueueADT
+ */
+public class ArrayQueue<T> implements QueueADT<T> {
+    /**
+     * The default capacity of this queue array
+     */
+    protected static final int DEFAULT_CAPACITY = 10;
+    /**
+     * The array containing the elements of this queue
+     */
+    protected T[] array;
+    /**
+     * The number of elements in this queue
+     */
+    protected int size;
+
+    /**
+     * Constructs an empty ArrayQueue with a default capacity.
+     */
+    public ArrayQueue() {
+        this(DEFAULT_CAPACITY);
+    }
+
+    /**
+     * Constructs an empty ArrayQueue with a specified initial capacity.
+     * 
+     * @param initialCapacity the initial capacity (Minimum value is 0)
+     */
+    @SuppressWarnings("unchecked")
+    public ArrayQueue(int initialCapacity) {
+        array = (T[]) new Object[(initialCapacity < 0) ? 0 : initialCapacity];
+        size = 0;
+    }
+
+    /**
+     * {@inheritDoc}
+     * 
+     * @throws NullPointerException if the element is null
+     */
+    @Override
+    public void enqueue(T element) throws NullPointerException {
+        if (element == null) throw new NullPointerException("Element is null");
+
+        if (size() == array.length) expandCapacity();
+        array[size++] = element;
+    }
+
+    /**
+     * {@inheritDoc}
+     * 
+     * @throws EmptyCollectionException if the queue is empty
+     */
+    @Override
+    public T dequeue() throws EmptyCollectionException {
+        T result = first();
+        for (int i = 0; i < size() - 1; i++) array[i] = array[i + 1];
+        array[--size] = null;
+
+        return result;
+    }
+
+    /**
+     * {@inheritDoc}
+     * 
+     * @throws EmptyCollectionException if the queue is empty
+     */
+    @Override
+    public T first() throws EmptyCollectionException {
+        if (isEmpty()) throw new EmptyCollectionException("List is empty");
+
+        return array[0];
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean isEmpty() {
+        return size() == 0;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public int size() {
+        return size;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String toString() {
+        StringBuilder result = new StringBuilder("[");
+        for (int i = 0; i < size(); i++) result.append(array[i]).append((i < size() - 1) ? ", " : "");
+        result.append("]");
+
+        return result.toString();
+    }
+
+    /**
+     * Expands the capacity of the array used to store the elements in this queue.
+     * If the current array length is 0 or 1, a new array with a capacity of the old length + 1 is created.
+     * Otherwise, a new array with a capacity increased by half of the current array's length is created.
+     * The elements from the current array are copied to the new array.
+     */
+    @SuppressWarnings("unchecked")
+    protected void expandCapacity() {
+        T[] newArray = (T[]) new Object[(array.length < 2) ? array.length + 1 : array.length + (array.length / 2)];
+        for (int i = 0; i < size(); i++) newArray[i] = array[i];
+        array = newArray;
+    }
+}
---
+++ datastructures/src/main/java/dev/joseafmoreira/abstractdatatypes/stacks/ArrayStack.java
@@ -0,0 +1,126 @@
+package dev.joseafmoreira.abstractdatatypes.stacks;
+
+import dev.joseafmoreira.exceptions.EmptyCollectionException;
+import pt.ipp.estg.ed.StackADT;
+
+/**
+ * Resizable-array implementation of the {@code StackADT} interface.
+ * 
+ * <h3>ArrayStack</h3>
+ * @param <T> the type of element stored in this stack
+ * @since 1.0
+ * @version 1.0
+ * @author joseafmoreira
+ * @see StackADT
+ */
+public class ArrayStack<T> implements StackADT<T> {
+    /**
+     * The default capacity of this stack array
+     */
+    protected static final int DEFAULT_CAPACITY = 10;
+    /**
+     * The array containing the elements of this stack
+     */
+    protected T[] array;
+    /**
+     * The number of elements in this stack
+     */
+    protected int size;
+
+    /**
+     * Constructs an empty ArrayStack with a default capacity.
+     */
+    public ArrayStack() {
+        this(DEFAULT_CAPACITY);
+    }
+
+    /**
+     * Constructs an empty ArrayStack with a specified initial capacity.
+     * 
+     * @param initialCapacity the initial capacity (Minimum value is 0)
+     */
+    @SuppressWarnings("unchecked")
+    public ArrayStack(int initialCapacity) {
+        array = (T[]) new Object[(initialCapacity < 0) ? 0 : initialCapacity];
+        size = 0;
+    }
+
+    /**
+     * {@inheritDoc}
+     * 
+     * @throws NullPointerException if the element is null
+     */
+    @Override
+    public void push(T element) throws NullPointerException {
+        if (element == null) throw new NullPointerException("Element is null");
+
+        if (size() == array.length) expandCapacity();
+        array[size++] = element;
+    }
+
+    /**
+     * {@inheritDoc}
+     * 
+     * @throws EmptyCollectionException if this stack is empty
+     */
+    @Override
+    public T pop() throws EmptyCollectionException {
+        T result = peek();
+        array[--size] = null;
+
+        return result;
+    }
+
+    /**
+     * {@inheritDoc}
+     * 
+     * @throws EmptyCollectionException if this stack is empty
+     */
+    @Override
+    public T peek() throws EmptyCollectionException {
+        if (isEmpty()) throw new EmptyCollectionException("List is empty");
+
+        return array[size() - 1];
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean isEmpty() {
+        return size() == 0;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public int size() {
+        return size;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String toString() {
+        StringBuilder result = new StringBuilder("[");
+        for (int i = 0; i < size(); i++) result.append(array[i]).append((i < size() - 1) ? ", " : "");
+        result.append("]");
+
+        return result.toString();
+    }
+
+    /**
+     * Expands the capacity of the array used to store the elements in this stack.
+     * If the current array length is 0 or 1, a new array with a capacity of the old length + 1 is created.
+     * Otherwise, a new array with a capacity increased by half of the current array's length is created.
+     * The elements from the current array are copied to the new array.
+     */
+    @SuppressWarnings("unchecked")
+    protected void expandCapacity() {
+        T[] newArray = (T[]) new Object[(array.length < 2) ? array.length + 1 : array.length + (array.length / 2)];
+        for (int i = 0; i < size(); i++) newArray[i] = array[i];
+        array = newArray;
+    }
+}
